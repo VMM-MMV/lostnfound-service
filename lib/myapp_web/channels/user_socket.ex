998f8a67753175be5d60cac5a71ac854d5ddcfad
vmm-mmv <mihai.vieru@isa.utm.md>
2025-09-21 20:52:53 +0300

    add jwt to websocket connection

--- lib/myapp_web/channels/user_socket.ex
+++ lib/myapp_web/channels/user_socket.ex
@@ -4,8 +4,14 @@ defmodule MyappWeb.UserSocket do
   ## Channels
   channel "lostnfound:*", MyappWeb.LostnfoundChannel
 
-  def connect(%{"token" => _token}, socket, _connect_info) do
-    {:ok, assign(socket, :user_id, "test-user")}
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case MyappWeb.Token.verify_and_validate(token) do
+      {:ok, %{"user_id" => user_id}} ->
+        {:ok, assign(socket, :user_id, user_id)}
+
+      {:error, _reason} ->
+        :error
+    end
   end
 
   def connect(_params, _socket, _connect_info), do: :error

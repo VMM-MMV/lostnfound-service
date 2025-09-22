4c6c7072fedabd5e12f01733e47c2c1615be25c9
vmm-mmv <mihai.vieru@isa.utm.md>
2025-09-22 21:05:37 +0300

    add jwt token directly to endpoint

---
+++ lib/myapp_web/controllers/token_controller.ex
@@ -0,0 +1,25 @@
+
+defmodule MyappWeb.TokenController do
+  use MyappWeb, :controller
+
+  alias Joken.Signer
+
+  def generate_token(conn, %{"user_id" => user_id}) do
+    # Create signer with hardcoded secret (for testing only)
+    signer = Signer.create("HS256", Application.get_env(:consumables_service, :jwt_secret, "secret"))
+
+    # Create payload
+    payload = %{
+      "user_id" => user_id,
+      "exp" => :os.system_time(:seconds) + 3600,
+      "iat" => :os.system_time(:seconds)
+    }
+
+    # Generate token
+    {:ok, token, _claims} = Joken.encode_and_sign(payload, signer)
+
+    conn
+    |> put_status(200)
+    |> json(%{token: token})
+  end
+end

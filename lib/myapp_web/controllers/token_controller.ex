f79e4ae9502346538b9e8b8dff02437f6cbb7dc3
vmm-mmv <mihai.vieru@isa.utm.md>
2025-09-22 21:14:19 +0300

    resolve import

--- lib/myapp_web/controllers/token_controller.ex
+++ lib/myapp_web/controllers/token_controller.ex
@@ -6,7 +6,7 @@ defmodule MyappWeb.TokenController do
 
   def generate_token(conn, %{"user_id" => user_id}) do
     # Create signer with hardcoded secret (for testing only)
-    signer = Signer.create("HS256", Application.get_env(:consumables_service, :jwt_secret, "secret"))
+    signer = Signer.create("HS256", Application.get_env(:myapp, :jwt_secret, "secret"))
 
     # Create payload
     payload = %{
